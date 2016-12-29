3ad43d8dcafb91b7a70790783300462660547691
mifaer <mifaer@gmail.com>
2016-12-29 16:45:46 +0200

    make layout and backpack with items

--- app/app.components.ts
+++ app/app.components.ts
@@ -8,28 +8,73 @@ import * as _ from "lodash";
     styleUrls: ['app.component.css']
 })
 export class AppComponent {
-    title = 'Angular 2Do';
+    title = 'Players inventory';
 
-    private todoList: any = [{
-        title: 'Купить продукты',
-        state: true,
-        date: new Date(),
-        id: '1'
-    }, {
-        title: 'Убрать хату',
-        state: false,
-        date: new Date(),
-        id: '2'
-    }, {
-        title: 'Чета еще...',
-        state: true,
-        date: new Date(),
-        id: '3'
+    private capacity: number = 50;
+    private backpack: any = [
+        {
+            title: 'bow',
+            position: '0px -432px'
+        },
+        {
+            title: 'arrows',
+            position: '-108px -432px'
+        },
+        {
+            title: 'mantle',
+            position: '-162px -486px'
+        },
+        {
+            title: 'pants',
+            position: '-216px -486px'
+        },
+        {
+            title: 'boot',
+            position: '-270px -486px'
+        },
+        {
+            title: 'gloves',
+            position: '-324px -486px'
+        },
+        {
+            title: 'sword',
+            position: '-324px -540px'
+        },
+        {
+            title: 'armorHard',
+            position: '0px -540px'
+        },
+        {
+            title: 'pantsHard',
+            position: '-54px -540px'
+        },
+        {
+            title: 'bootHard',
+            position: '-108px -540px'
+        },
+        {
+            title: 'glovesHard',
+            position: '-216px -540px'
+        },
+        {
+            title: 'shield',
+            position: '-270px -540px'
         }
     ];
 
-    private deleteTodo(todo: any) {
-        console.log(todo);
-        this.todoList = this.todoList.filter((item: any) => item.id !== todo.id);
+    constructor() {
+        const diff = this.capacity - this.backpack.length;
+        for(let i=0; i<diff; i++) {
+            this.backpack.push({});
+        }
     }
+
+    private attributes: any = {
+        strenght: '100%',
+        dexterity: '100%',
+        intelligence: '100%',
+        vitality: '100%',
+        armor: '100%',
+        damage: '100%'
+    };
  }
